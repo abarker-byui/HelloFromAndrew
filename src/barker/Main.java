012be408598618363073e3d0c3d4bf224ae80221
thomasen007 <thomas.elliott.nielson@gmail.com>
2019-05-18 09:44:46 -0700

    Update from Thomas

--- src/barker/Main.java
+++ src/barker/Main.java
@@ -3,6 +3,6 @@ package barker;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, my name is Andrew.");
+	    System.out.println("Hello, my name is Andrew, my name is Thomas.");
     }
 }
